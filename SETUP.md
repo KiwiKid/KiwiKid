cd248d194c195b3175d2dec24b34afd8d11d00ed
KiwiKid <KiwiKid@users.noreply.github.com>
2024-01-15 10:33:24 +1300

    add a-h/templating

---
+++ SETUP.md
@@ -0,0 +1,7 @@
+```
+# Generate templates
+templ generate --watch
+
+# Output templates
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+)
+
+func main() {
+	outputDir := "./output"
+
+	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+		err := os.MkdirAll(outputDir, 0755) // 0755 is a common permission setting (read and execute access for everyone, full access for the owner)
+		if err != nil {
+			log.Fatalf("failed to create output directory: %v", err)
+		}
+	}
+
+	f, err := os.Create(outputDir + "/home.html")
+	if err != nil {
+		log.Fatalf("failed to create output file: %v", err)
+	}
+
+	err = home().Render(context.Background(), f)
+	if err != nil {
+		log.Fatalf("failed to write output file: %v", err)
+	}
+
+}
